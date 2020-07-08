94e35bb01643445150a368a2fe30e98aaa1d5dc3
Caio Henrique <caio@ganex.com.br>
2020-07-08 17:02:37 -0300

    Add cloudfront_id output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@ output "bucket_name" {
   value       = aws_s3_bucket.name.id
 }
 
+output "cloudfront_id" {
+  description = "The identifier for the cloudfront distribution"
+  value       = aws_cloudfront_distribution.default.id
+}
