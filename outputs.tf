a06b8e112f5a9ac334f4bd9f9de0d69eae109bbe
Caio Henrique <caio@ganex.com.br>
2020-10-22 14:39:38 -0300

    Add s3 bucket arn output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "cloudfront_id" {
   description = "The identifier for the cloudfront distribution"
   value       = aws_cloudfront_distribution.default.*.id
 }
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.name.arn
+  description = "The ARN of the S3 Bucket project."
+}
