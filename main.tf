7b8f18ef0706b23eab804a865267db19e86b2d2c
Caio Henrique <caio@ganex.com.br>
2021-02-17 17:49:53 -0300

    Fix origin_website policy

--- main.tf
+++ main.tf
@@ -84,10 +84,6 @@ data "aws_iam_policy_document" "origin_website" {
     sid       = "PublicReadGetObject"
     actions   = ["s3:GetObject"]
     resources = ["arn:aws:s3:::${var.name}${var.origin_path}*"]
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
   }
 }
 
